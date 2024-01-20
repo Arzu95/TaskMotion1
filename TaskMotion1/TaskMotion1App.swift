b7ae191455b87701fb7a8c11a6209256aaf6a757
Muhammad Arzu <muhammadarzu3432@gmail.com>
2024-01-20 10:51:11 +0700

    Initial Commit

---
+++ TaskMotion1/TaskMotion1App.swift
@@ -0,0 +1,17 @@
+//
+//  TaskMotion1App.swift
+//  TaskMotion1
+//
+//  Created by Muhammad Arzu on 20/01/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskMotion1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
